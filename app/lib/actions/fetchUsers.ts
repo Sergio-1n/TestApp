0524d767ffe02380f3b3b872f3777d9b04f25146
Sergio-1n <skuznetcov150@gmail.com>
2024-12-01 18:22:07 +0100

    port3000

--- app/lib/actions/fetchUsers.ts
+++ app/lib/actions/fetchUsers.ts
@@ -1,6 +1,6 @@
 // lib/actions/fetchUsers.ts
 export async function fetchUsers() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`, {
+  const response = await fetch(`http://localhost:3000/api/users`, {
     cache: 'no-store',
   });
 
